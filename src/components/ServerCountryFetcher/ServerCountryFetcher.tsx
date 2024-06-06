1afa3835fe2ec84b1799b29c249be5e1a0b6ed58
Ivan L <postscriptum.no@gmail.com>
2024-06-06 22:57:11 +0300

    Cover a bug issue when countries info load failed
    - Add reload button and description of the problem in case of countries info load fail
    - Remove unused code

--- src/components/ServerCountryFetcher/ServerCountryFetcher.tsx
+++ src/components/ServerCountryFetcher/ServerCountryFetcher.tsx
@@ -4,8 +4,6 @@ import CountriesPage from "../CountriesPage";
 const ServerCountryFetcher = async () => {
   const countries = await fetchAllCountries();
 
-  if (!countries) return null;
-
   return <CountriesPage countries={countries} />;
 };
 
