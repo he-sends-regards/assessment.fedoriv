470cca629bd1c0c7dbff6c85e4e08a2b86c0df83
Ivan L <postscriptum.no@gmail.com>
2024-06-06 22:39:55 +0300

    Update country details test

--- src/components/CountryDetails/CountryDetails.test.tsx
+++ src/components/CountryDetails/CountryDetails.test.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import CountryDetails from "./CountryDetails";
-import { mockCountryFinland } from "@/tests/fixtures/";
+
+import { useRouter } from "next/navigation";
+import { mockCountryFinland } from "@/tests/fixtures";
+
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+}));
+
+const mockPush = jest.fn();
+
+(useRouter as jest.Mock).mockReturnValue({
+  push: mockPush,
+});
 
 describe("CountryDetails", () => {
-  test("renders the country details correctly", () => {
+  test("renders country details", () => {
     render(<CountryDetails country={mockCountryFinland} />);
 
     expect(screen.getByText("Finland")).toBeInTheDocument();
@@ -16,28 +29,16 @@ describe("CountryDetails", () => {
     expect(screen.getByText(".fi")).toBeInTheDocument();
     expect(screen.getByText("Euro")).toBeInTheDocument();
     expect(screen.getByText("Finnish, Swedish")).toBeInTheDocument();
-    expect(screen.getByText("Border Countries:")).toBeInTheDocument();
     expect(screen.getByText("SWE")).toBeInTheDocument();
     expect(screen.getByText("NOR")).toBeInTheDocument();
   });
 
-  test("renders the flag image with correct alt text", () => {
+  test("navigates to border country on click", () => {
     render(<CountryDetails country={mockCountryFinland} />);
 
-    const flagImage = screen.getByRole("img", { name: "Flag of Finland" });
-    expect(flagImage).toBeInTheDocument();
-    expect(flagImage).toHaveAttribute("src");
-    expect(flagImage.getAttribute("src")).toContain(
-      encodeURIComponent(mockCountryFinland.flags.svg)
-    );
-  });
+    const borderCountryButton = screen.getByText("SWE");
+    fireEvent.click(borderCountryButton);
 
-  test("renders the border countries with correct href", () => {
-    render(<CountryDetails country={mockCountryFinland} />);
-
-    const borderCountryLinks = screen.getAllByRole("link");
-    expect(borderCountryLinks).toHaveLength(2);
-    expect(borderCountryLinks[0]).toHaveAttribute("href", "/country/SWE");
-    expect(borderCountryLinks[1]).toHaveAttribute("href", "/country/NOR");
+    expect(mockPush).toHaveBeenCalledWith("/country/SWE");
   });
 });
