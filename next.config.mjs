ef6430da6cca4bd0a143b56b2a0907600812e351
Ivan L <postscriptum.no@gmail.com>
2024-06-06 22:40:11 +0300

    Fix babel configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["flagcdn.com", "upload.wikimedia.org"],
   },
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 export default nextConfig;
